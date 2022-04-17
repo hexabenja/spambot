eb4a794b13662cde4e7a468130e6071960bd2585
benja <benjamin.n.ign@gmail.com>
2022-04-17 12:21:35 -0400

    Agregué script ventana, agregué Carga módulo tk, carga ttk, Nombre de la variable Tk, Tamaño de la ventana, color de fondo de ventana black, titulo de ventana, favicon, a continuación script Spambot y luego botón de salir de ventana y loop para mantener la ventana abierta

--- SPAMBOT.py
+++ SPAMBOT.py
@@ -1,7 +1,31 @@
+from multiprocessing.spawn import spawn_main
 import pyautogui
+import tkinter
 import time
+# Script python
 f = open('spam', 'r')
 time.sleep(5)
 for word in f:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
+# Script Ventana
+
+from tkinter import *    # Carga módulo tk (widgets estándar)
+from tkinter import ttk  # Carga ttk (para widgets nuevos 8.5+)
+raiz = Tk() # Nombre de la variable TK
+raiz.geometry('300x200') # anchura x altura
+raiz.configure(bg = 'black') # Color de fondo de ventana
+raiz.title('SPAMBOT') # Titulo de Ventana
+raiz.iconbitmap("floppa.ico") # Favicon
+    # Script python
+    f = open('spam', 'r')
+    time.sleep(5)
+    for word in f:
+        pyautogui.typewrite(word)
+        pyautogui.press("enter")
+ttk.Button(raiz, text='Salir', command=quit).pack(side=BOTTOM) # Boton de salida
+raiz.mainloop() # Para mantener la ventana abierta en bucle
+
+Texto de Spam
+
+Texto de spawn_main
