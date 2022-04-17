894c80ec83e95ca5435f0d9e1b139ed9cf587efb
benja <benjamin.n.ign@gmail.com>
2022-04-17 12:42:04 -0400

    Borré el codigo de script spambot dentro de Ventana

--- SPAMBOT.py
+++ SPAMBOT.py
@@ -17,15 +17,6 @@ raiz.geometry('300x200') # anchura x altura
 raiz.configure(bg = 'black') # Color de fondo de ventana
 raiz.title('SPAMBOT') # Titulo de Ventana
 raiz.iconbitmap("floppa.ico") # Favicon
-    # Script python
-    f = open('spam', 'r')
-    time.sleep(5)
-    for word in f:
-        pyautogui.typewrite(word)
-        pyautogui.press("enter")
 ttk.Button(raiz, text='Salir', command=quit).pack(side=BOTTOM) # Boton de salida
 raiz.mainloop() # Para mantener la ventana abierta en bucle
 
-Texto de Spam
-
-Texto de spawn_main
