4dd2b7d4a7c58ef332952d8d93bb917ec95a2ae1
benja <benjamin.n.ign@gmail.com>
2022-04-17 13:31:17 -0400

    Desactivé script de Spambot, cambié tamaño de ventana de 300x200 a 600x600, agregué un diseño de Groove a la venta con miFrame de color Gris, le agregué texto color negro a la ventana con Label miFrame con fuente Calibri Light 18, posicionado en x200, y10 y borde de diseño con bf45

--- SPAMBOT.py
+++ SPAMBOT.py
@@ -2,21 +2,29 @@ from multiprocessing.spawn import spawn_main
 import pyautogui
 import tkinter
 import time
-# Script python
-f = open('spam', 'r')
-time.sleep(5)
-for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
-# Script Ventana
 
+# Script python
+# f = open('spam', 'r')
+# time.sleep(5)
+# for word in f:
+    # pyautogui.typewrite(word)
+    # pyautogui.press("enter")
+
+# Script Ventana
 from tkinter import *    # Carga módulo tk (widgets estándar)
 from tkinter import ttk  # Carga ttk (para widgets nuevos 8.5+)
 raiz = Tk() # Nombre de la variable TK
-raiz.geometry('300x200') # anchura x altura
+raiz.geometry('600x600') # anchura x altura
 raiz.configure(bg = 'black') # Color de fondo de ventana
 raiz.title('SPAMBOT') # Titulo de Ventana
 raiz.iconbitmap("floppa.ico") # Favicon
+miFrame=Frame()
+miFrame.pack()
+Label(miFrame, text= "Spambot", fg="black", font=("Calibri Light", 18)).place(x=200, y=10)
+miFrame.config(bg="gray")
+miFrame.config(width=600, height=550)
+miFrame.config(bd=45)
+miFrame.config(relief="groove")
 ttk.Button(raiz, text='Salir', command=quit).pack(side=BOTTOM) # Boton de salida
 raiz.mainloop() # Para mantener la ventana abierta en bucle
 
