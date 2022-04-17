a9a4da71fb3f15655db78d472cdba386cd53e57d
benja <benjamin.n.ign@gmail.com>
2022-04-17 19:07:48 -0400

    Movi el script de spambot a script.py, intente abrir el archivo script con def start pero no resulto

--- SPAMBOT.py
+++ SPAMBOT.py
@@ -1,7 +1,9 @@
 from multiprocessing.spawn import spawn_main
+import pydoc
 import pyautogui
 import tkinter
 import time
+import os
 
 # Script python
 # f = open('spam', 'r')
@@ -13,6 +15,9 @@ import time
 # Script Ventana
 from tkinter import *    # Carga módulo tk (widgets estándar)
 from tkinter import ttk  # Carga ttk (para widgets nuevos 8.5+)
+
+def start(self):
+        self.im = pydoc.open(os.getcwd() + "assets/Script.py")
 
 raiz = Tk() # Nombre de la variable TK
 raiz.geometry('600x600') # anchura x altura
@@ -26,7 +31,7 @@ miFrame.config(bg="gray")
 miFrame.config(width=600, height=550)
 miFrame.config(bd=45)
 miFrame.config(relief="groove")
-botonst= tkinter.Button(text="Iniciar script").pack(side=BOTTOM)
+botonst= tkinter.Button(text="Iniciar script", command=start).pack(side=BOTTOM)
 ttk.Button(raiz, text='Salir', command=quit).pack(side=BOTTOM) # Boton de salida
 raiz.mainloop() # Para mantener la ventana abierta en bucle
 
